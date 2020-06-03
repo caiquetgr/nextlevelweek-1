7c1eabb7a45481ed5c68de6b48af74b5b1f3e6a2
Caique Borges <caiquetgr@gmail.com>
2020-06-03 17:33:47 -0300

    Finalizada terceira aula, app react terminado

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,9 +6,9 @@ import path from 'path';
 const app = express();
 
 app.use(express.json());
-app.use(routes);
 app.use(cors());
 app.listen(3333);
+app.use(routes);
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 /* 
